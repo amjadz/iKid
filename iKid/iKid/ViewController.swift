824913f7f00a8c7f56092cefe2d55e0066e5db81
Zubair Amjad <amjadz@uw.edu>
2018-10-22 09:43:09 -0700

    Starter files

---
+++ iKid/iKid/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iKid
+//
+//  Created by Zubair Amjad on 10/22/18.
+//  Copyright © 2018 Zubair Amjad. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
